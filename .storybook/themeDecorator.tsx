d39dc231efc3e1420c6d0501013e996a4f094fdd
Le Bao Trung Ngo <ngolebaotrung@gmail.com>
2025-03-08 17:10:11 +1000

    add color palette

--- .storybook/themeDecorator.tsx
+++ .storybook/themeDecorator.tsx
@@ -1,11 +1,13 @@
 // themeDecorator.tsx
-import React from "react";
 import { Decorator } from "@storybook/react";
 import { ThemeProvider } from "@mui/material";
-import { myTheme } from "../src/theme/theme";
+import { useTheme } from "../src/theme/theme";
 
-export const withThemeProvider: Decorator = (Story, context) => (
-  <ThemeProvider theme={myTheme}>
-    <Story {...context} />
-  </ThemeProvider>
-);
+export const withThemeProvider: Decorator = (Story, context) => {
+  const { theme } = useTheme();
+  return (
+    <ThemeProvider theme={theme}>
+      <Story {...context} />
+    </ThemeProvider>
+  );
+};
